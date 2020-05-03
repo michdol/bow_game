0b27a7604a80f77f0039cdfb73bfb3d47731b651
michdol <michal.dolbniak@gmail.com>
2020-05-03 18:23:28 +0900

    Character's speed adjustments

--- app.py
+++ app.py
@@ -5,114 +5,110 @@ from arrow import Arrow
 from character import Character
 from aim_line import AimLine
 from constants import (
-	FLOOR_Y,
-	CHARACTER_WIDTH,
-	CHARACTER_HEIGHT,
-	SCREEN_WIDTH,
-	SCREEN_HEIGHT
+  FLOOR_Y,
+  CHARACTER_WIDTH,
+  CHARACTER_HEIGHT,
+  SCREEN_WIDTH,
+  SCREEN_HEIGHT
 )
 from colors import (
-	WHITE,
-	BLACK,
-	BLUE,
-	GREEN,
-	RED,
+  BLACK,
+  BLUE,
+  GREEN,
+  RED,
 )
-
-
 
 
 class App(object):
-	def __init__(self):
-		self._running = True
-		self.clock = pygame.time.Clock()
-		self.size = self.width, self.height = SCREEN_WIDTH, SCREEN_HEIGHT
-		self.all_sprites = pygame.sprite.Group()
-		self.character = None
-		self.aim_line = None
+  def __init__(self):
+    self._running = True
+    self.clock = pygame.time.Clock()
+    self.size = self.width, self.height = SCREEN_WIDTH, SCREEN_HEIGHT
+    self.all_sprites = pygame.sprite.Group()
+    self.character = None
+    self.aim_line = None
 
-	def on_init(self):
-		pygame.init()
-		self.screen = pygame.display.set_mode(self.size)
-		self._running = True
-		self.character = Character(RED, CHARACTER_WIDTH, CHARACTER_HEIGHT, self.screen)
-		self.aim_line = AimLine(RED, CHARACTER_WIDTH, CHARACTER_HEIGHT, self.screen, self.character)
-		self.arrows = []
-		self.character.rect.x = 100
-		self.character.draw_character_on_the_floor()
-		self.all_sprites.add(self.character)
-		self.click_position = None
-		self.current_arrow = None
-		self.arrows = []
-		self.t = 0
+  def on_init(self):
+    pygame.init()
+    self.screen = pygame.display.set_mode(self.size)
+    self._running = True
+    self.character = Character(RED, CHARACTER_WIDTH, CHARACTER_HEIGHT, self.screen)
+    self.aim_line = AimLine(RED, CHARACTER_WIDTH, CHARACTER_HEIGHT, self.screen, self.character)
+    self.arrows = []
+    self.character.rect.x = 100
+    self.character.draw_character_on_the_floor()
+    self.all_sprites.add(self.character)
+    self.click_position = None
+    self.current_arrow = None
+    self.arrows = []
+    self.t = 0
 
-	def on_event(self, event):
-		mouse = pygame.mouse.get_pressed()
-		if event.type == pygame.QUIT:
-			self._running = False
-		elif event.type == pygame.KEYDOWN:
-			if event.key == K_ESCAPE:
-				self._running = False
-			elif event.key == K_SPACE:
-				self.character.jump()
-		elif event.type == pygame.MOUSEBUTTONDOWN:
-			if mouse[0]:
-				self.click_position = event.pos
-				self.all_sprites.add(self.create_arrow(self.click_position))
-		elif event.type == pygame.MOUSEBUTTONUP:
-			if not mouse[0]:
-				self.current_arrow.release(pygame.mouse.get_pos())
-				self.click_position = None
-				self.current_arrow = None
+  def on_event(self, event):
+    mouse = pygame.mouse.get_pressed()
+    if event.type == pygame.QUIT:
+      self._running = False
+    elif event.type == pygame.KEYDOWN:
+      if event.key == K_ESCAPE:
+        self._running = False
+      elif event.key == K_SPACE:
+        self.character.jump()
+    elif event.type == pygame.MOUSEBUTTONDOWN:
+      if mouse[0]:
+        self.click_position = event.pos
+        self.all_sprites.add(self.create_arrow(self.click_position))
+    elif event.type == pygame.MOUSEBUTTONUP:
+      if not mouse[0]:
+        self.current_arrow.release(pygame.mouse.get_pos())
+        self.click_position = None
+        self.current_arrow = None
 
-	def on_loop(self):
-		keys = pygame.key.get_pressed()
-		if keys[pygame.K_a]:
-			self.character.move_left()
-		if keys[pygame.K_d]:
-			self.character.move_right()
-		if self.character.is_jumping:
-			self.character.perform_jump()
-		for arrow in self.arrows:
-			arrow.set_center()
-			arrow.update()
-		self.arrows = [arrow for arrow in self.arrows if not arrow.stopped]
+  def on_loop(self):
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_a]:
+      self.character.move_left()
+    if keys[pygame.K_d]:
+      self.character.move_right()
+    if self.character.is_jumping:
+      self.character.perform_jump()
+    for arrow in self.arrows:
+      arrow.set_center()
+      arrow.update()
+    self.arrows = [arrow for arrow in self.arrows if not arrow.stopped]
 
-	def on_render(self):
-		self.screen.fill(GREEN)
-		# Draw floor
-		pygame.draw.rect(self.screen, BLACK, [0, FLOOR_Y, self.width, 1])
-		self.all_sprites.draw(self.screen)
-		if self.click_position:
-			self.aim_line.draw(self.click_position)
-		pygame.display.flip()
+  def on_render(self):
+    self.screen.fill(GREEN)
+    # Draw floor
+    pygame.draw.rect(self.screen, BLACK, [0, FLOOR_Y, self.width, 1])
+    self.all_sprites.draw(self.screen)
+    if self.click_position:
+      self.aim_line.draw(self.click_position)
+    pygame.display.flip()
 
-	def on_cleanup(self):
-		pygame.quit()
+  def on_cleanup(self):
+    pygame.quit()
 
-	def on_execute(self):
-		if self.on_init() == False:
-			self._running = False
+  def on_execute(self):
+    if self.on_init() == False:
+      self._running = False
 
-		while self._running:
-			for event in pygame.event.get():
-				self.on_event(event)
-			self.on_loop()
-			self.on_render()
-			self.clock.tick(60)
-		self.on_cleanup()
+    while self._running:
+      for event in pygame.event.get():
+        self.on_event(event)
+      self.on_loop()
+      self.on_render()
+      self.clock.tick(60)
+    self.on_cleanup()
 
-	def create_arrow(self, click_position):
-		arrow =  Arrow(BLUE, self.screen, self.character, click_position)
-		self.current_arrow = arrow
-		self.arrows.append(arrow)
-		return arrow
+  def create_arrow(self, click_position):
+    arrow = Arrow(BLUE, self.screen, self.character, click_position)
+    self.current_arrow = arrow
+    self.arrows.append(arrow)
+    return arrow
 
 
 if __name__ == "__main__":
-	theApp = App()
-	theApp.on_execute()
-
+  theApp = App()
+  theApp.on_execute()
 
 # https://stackoverflow.com/questions/44465783/how-to-make-arrow-shoot-in-direction-of-mouse
 
--- character.py
+++ character.py
@@ -2,61 +2,60 @@ from pygame import Surface, draw, mouse
 from pygame.sprite import Sprite
 
 from constants import (
-	CHARACTER_WIDTH,
-	CHARACTER_HEIGHT,
-	CHARACTER_SPEED,
-	CHARACTER_JUMP_FRAMES,
-	FLOOR_Y,
+  CHARACTER_HEIGHT,
+  CHARACTER_SPEED,
+  CHARACTER_JUMP_FRAMES,
+  FLOOR_Y,
 )
 
 
 class Character(Sprite):
-	def __init__(self, color, width, height, screen):
-		Sprite.__init__(self)
-		# Pass in the color of the car, and its x and y position, width and height.
-		# Set the background color and set it to be transparent
-		self.color = color
-		self.image = Surface([width, height])
-		self.image.fill(color)
-		draw.rect(self.image, color, [0, 0, width, height])
-		# Instead we could load a proper pciture of a car..	.
-		# self.image = pygame.image.load("car.png").convert_alpha()
-		# Fetch the rectangle object that has the dimensions of the image.
-		self.rect = self.image.get_rect()
+  def __init__(self, color, width, height, screen):
+    Sprite.__init__(self)
+    # Pass in the color of the car, and its x and y position, width and height.
+    # Set the background color and set it to be transparent
+    self.color = color
+    self.image = Surface([width, height])
+    self.image.fill(color)
+    draw.rect(self.image, color, [0, 0, width, height])
+    # Instead we could load a proper pciture of a car.. .
+    # self.image = pygame.image.load("car.png").convert_alpha()
+    # Fetch the rectangle object that has the dimensions of the image.
+    self.rect = self.image.get_rect()
 
-		self.is_jumping = False
-		self.jumpCnt = CHARACTER_JUMP_FRAMES
-		self.screen = screen
+    self.is_jumping = False
+    self.jumpCnt = CHARACTER_JUMP_FRAMES
+    self.screen = screen
 
-	def get_center(self):
-		return self.rect.center
+  def get_center(self):
+    return self.rect.center
 
-	def collidepoint(self, position):
-		return self.rect.collidepoint(position) == 1
+  def collidepoint(self, position):
+    return self.rect.collidepoint(position) == 1
 
-	def move_left(self):
-		self.rect.move_ip(-1 * CHARACTER_SPEED, 0)
+  def move_left(self):
+    self.rect.move_ip(-CHARACTER_SPEED, 0)
 
-	def move_right(self):
-		self.rect.move_ip(1 * CHARACTER_SPEED, 0)
+  def move_right(self):
+    self.rect.move_ip(CHARACTER_SPEED, 0)
 
-	def jump(self):
-		if not self.is_jumping:
-			self.is_jumping = True
+  def jump(self):
+    if not self.is_jumping:
+      self.is_jumping = True
 
-	def perform_jump(self):
-		if self.jumpCnt >= -CHARACTER_JUMP_FRAMES:
-			self.rect.y -= (self.jumpCnt * abs(self.jumpCnt)) * 0.5
-			self.jumpCnt -= 1
-		else:
-			self.jumpCnt = CHARACTER_JUMP_FRAMES
-			self.draw_character_on_the_floor()
-			self.is_jumping = False
+  def perform_jump(self):
+    if self.jumpCnt >= -CHARACTER_JUMP_FRAMES:
+      self.rect.y -= (self.jumpCnt * abs(self.jumpCnt)) * 0.5
+      self.jumpCnt -= 1
+    else:
+      self.jumpCnt = CHARACTER_JUMP_FRAMES
+      self.draw_character_on_the_floor()
+      self.is_jumping = False
 
-	def draw_character_on_the_floor(self):
-		self.rect.y = FLOOR_Y - CHARACTER_HEIGHT
+  def draw_character_on_the_floor(self):
+    self.rect.y = FLOOR_Y - CHARACTER_HEIGHT
 
-	def draw_mouse(self):
-		mouse_pos = mouse.get_pos()
-		pos = (self.rect.x, self.rect.y)
-		draw.line(self.screen, self.color, pos, mouse_pos, 5)		
+  def draw_mouse(self):
+    mouse_pos = mouse.get_pos()
+    pos = (self.rect.x, self.rect.y)
+    draw.line(self.screen, self.color, pos, mouse_pos, 5)
